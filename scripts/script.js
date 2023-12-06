ef34f0b397e17482706194caa8185eadec902d7c
Knightmare12321 <qiu4135@gmail.com>
2023-12-05 23:48:51 -0800

    content

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,39 @@
 const game = {
     gameIsRunning : false,
     gameWasRunning : false,
+    playerName: null,
+    playerScore: 0,
+    playerLife: 3,
+    arrowLength: 4,
+    arrowRed: [],
+    arrowGreen: [],
     currentScreen : "splash-screen",
+    
     switchScreen: function(screenID) {
         $(".splash-screen").css("display","none");
         $(".game-screen").css("display","none");
         $(".gameover-screen").css("display","none");
         $(".container").removeClass("containerPlay");
-        $("#buttonQuit").css("visibility", "visible");
-        $("#buttonHelp").css("visibility", "visible");
+        $("#buttonQuit").css("display", "none");
+        $("#buttonHelp").css("display", "inline");
         if (screenID === "splash-screen") {
             this.currentScreen = "splash-screen";
             $(".splash-screen").css("display","block");
-            $("#buttonQuit").css("visibility", "hidden");
-            $("#buttonHelp").attr("data-bs-target", "#splashModal");
+            $("#buttonQuit").css("display", "none");
         }
         else if (screenID === "game-screen") {
             this.currentScreen = "game-screen";
             $(".game-screen").css("display","block");
-            $("#buttonHelp").attr("data-bs-target", "#gameModal");
+            $("#buttonQuit").css("display", "inline");
+            $("#buttonPause").css("display", "inline");
+            $("#buttonHelp").css("display", "none");
         }
         else {
             this.currentScreen = "gameover-screen";
             $(".gameover-screen").css("display","block");
-            $("#buttonQuit").css("visibility", "hidden");
-            $("#buttonHelp").css("visibility", "hidden");
+            $("#buttonQuit").css("display", "none");
+            $("#buttonPause").css("display", "none");
+            $("#buttonHelp").css("display", "none");
         }
         
     },
@@ -37,12 +46,23 @@ const game = {
     }
 }
 
+$("#buttonQuit").css("display", "none");
+$("#buttonPause").css("display", "none");
+
 $("#buttonStart").on("click", () => {
-    game.switchScreen("game-screen");
+    var input = $("#usernameInput").val().trim();
+    if (input === "") {
+        alert("Please enter a non-empty username");
+    }
+    else {
+        game.playerName = input;
+        game.switchScreen("game-screen");
+    }
 })
 
 $("#buttonPlay").on("click", () => {
     $(".container").toggleClass("containerPlay");
+
     game.gameIsRunning ? game.gameIsRunning = false : game.gameIsRunning = true;
 })
 
@@ -55,23 +75,25 @@ $("#buttonQuit").on("click", () => {
     game.switchScreen("splash-screen");
 })
 
+
+$("#buttonHelp").on("click", () => {
+    game.displayHelp();
+})
+
+
 $("#buttonGameoverPlay").on("click", () => {
     game.switchScreen("game-screen")
 })
 
 $("#buttonGameoverQuit").on("click", () => {
     game.switchScreen("splash-screen")
-})
+}),
 
-$("#buttonHelp").on("click", () => {
-    game.displayHelp();
-})
-
-$("#gameModal").on("hidden.bs.modal", function () {
-    game.displayHelp();
-});
-
-$("#buttonInfo").on("click", () => {
-    $("#splashModal").modal("hide");
-    $("#gameModal").modal("show");
-})
+generateArrowIndexs = (size) => {
+    const randomArray = [];
+    for (let i = 0; i < size; i++) {
+        const randomInt = Math.floor(Math.random()*4) + 1;
+        randomArray.push(randomInt);
+    }
+    return randomArray;
+}
